3152a6d82216c576f01da8a8e3889c574155bb88
Reginald Eli Deinla <radeinla@softwarelab7.com>
2016-03-15 22:38:07 +0800

    Illustrate similar scenario to rocketmiles code.
    It shows that the subclass is not being marshalled by registered
    closure marshaller for the parent class. Incidentally the object
    being marshalled has a grailsApplication and is causing all sorts of
    errors when its being marshalled.

--- src/integration-test/groovy/demo/MarshallerSpec.groovy
+++ src/integration-test/groovy/demo/MarshallerSpec.groovy
@@ -1,5 +1,6 @@
 package demo
 
+import grails.core.GrailsApplication
 import grails.test.mixin.integration.Integration
 import groovyx.net.http.RESTClient
 import org.apache.commons.io.IOUtils
@@ -10,52 +11,15 @@ import grails.converters.JSON
 
 @Integration
 class MarshallerSpec extends Specification {
-    
+
+    GrailsApplication grailsApplication
+
     void "test marshalling subclass"() {
         when:
-        def sub = new PojoSubclass()
+        assert grailsApplication != null
+        def sub = new PojoSubclass(grailsApplication: grailsApplication)
         
         then:
-        new JSON(sub).toString(true).contains("PojoSubclass")
-    }
-
-    void "test marshaller"() {
-        setup:
-        def client = new RESTClient('http://localhost:8080/')
-
-        when:
-        def response = client.get(path: 'demo/index.json')
-
-        then:
-        response.status == HttpStatus.OK.value()
-        response.contentType == 'application/json'
-        response.data.first == null
-        response.data.second == null
-        response.data.widget == null
-
-        when:
-        response = client.get(path: 'demo/index.json',
-                              query: [firstProperty: 'alpha', secondProperty: 'beta'])
-
-        then:
-        response.status == HttpStatus.OK.value()
-        response.contentType == 'application/json'
-        response.data.first == 'alpha'
-        response.data.second == 'beta'
-        response.data.widget == null
-
-        when:
-        response = client.get(path: 'demo/generateJson.json',
-                query: [firstProperty: 'one', secondProperty: 'two'])
-
-        then:
-        response.status == HttpStatus.OK.value()
-        response.contentType == 'text/plain'
-
-        when:
-        def responseText = IOUtils.toString(response.data)
-
-        then:
-        responseText == 'Generated JSON: {"first":"one","second":"two","widget":null,"klazz":"demo.SomePojo"}'
+        new JSON(sub).toString() == '{"first":"one","second":"two","widget":null,"klazz":"demo.SomePojo"}'
     }
 }
--- src/main/groovy/demo/PojoSubclass.groovy
+++ src/main/groovy/demo/PojoSubclass.groovy
@@ -1,5 +1,7 @@
 package demo
 
+import grails.core.GrailsApplication
+
 class PojoSubclass extends SomePojo {
-  
+    GrailsApplication grailsApplication
 }
